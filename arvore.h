95d6069f965d35718cd29dc23c4eee1d8a1aafe8
Guilherme Monteiro de Oliveira <gui_monteirooliveira@hotmail.com>
2016-05-23 19:55:51 -0300

    Lib
    biblioteca arvore binaria

---
+++ arvore.h
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct no No;
+struct no{
+	int dado;
+	No *esquerda;
+	No *direita;
+};
+
+//--------------------CRIA-VAZIO-------------------------------
+int vazia(No* empty){
+	return empty==NULL;
+}
+
+//-------------------------IMPRIME--------------------------------
+void imprime(No* a){
+	if (!vazia(a)){
+		printf("%d ", a->dado);		//mostra raiz
+		imprime(a->esquerda);	//mostra lado esquerdo
+		imprime(a->direita);		//mostra lado direito
+	}
+}
+
+//-------------------------INSERE--------------------------------
+void inserir(No** endPonteiroNo, int x){
+	if (*endPonteiroNo == NULL){
+		*endPonteiroNo = (No*)malloc(sizeof(No));
+		(*endPonteiroNo)->dado = x;
+		(*endPonteiroNo)->esquerda = NULL;
+		(*endPonteiroNo)->direita = NULL;
+	}
+	else{
+		if (x < (*endPonteiroNo)->dado){
+			inserir(&(*endPonteiroNo)->esquerda, x);
+		}		
+		else {
+			inserir(&(*endPonteiroNo)->direita, x);
+		}
+	}
+}
+
+//-------------------BUSCA-------------------------------------
+int busca(No* a, int num){
+	if(a==NULL){
+		//puts("valor nao existe \n");
+		return 0;
+	}
+	else{
+		if(num==a->dado){
+			printf("\n%d\n",a->dado);
+			return 1;		
+		}
+		else if(busca(a->esquerda, num)){
+			return 1;
+		}
+		else if(busca(a->direita, num)){
+			return 1;
+		}	
+	}
+}
+
+//-------------------------LIBERA MEMÓRIA----------------------
+No* liberaMemoria(No* a){
+	if (!vazia(a)){
+		liberaMemoria(a->esquerda);	
+		liberaMemoria(a->direita);
+		free(a);			
+	}
+	return NULL;
+}
+
+//--------------------------REMOVE----------------------------
+
+
+
+
+
+
